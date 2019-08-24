d853ab581e6ee55ec897acf29e584db0d6caa6f4
Razor7w <seba.carroza@gmail.com>
2019-08-23 23:52:13 -0400

    Import libraries and create function "getInfoApi"

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = 'http://192.168.43.225/api/'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getInfoApi(){
+    return this.http.get(`${this.baseUrl}`);
+  }
 }
